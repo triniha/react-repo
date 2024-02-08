7fff12a4c377bd206941d66a9aaa6d30d31a739c
triniha <116326940+triniha@users.noreply.github.com>
2024-02-08 11:03:53 +0530

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+// src/HelloWorld.js
+import React from 'react';
+
+const HelloWorld = ({ name }) => {
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
